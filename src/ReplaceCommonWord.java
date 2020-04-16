2162ffab6a27aec371fa8573ff39dc4e54765329
azdoolittle <drewdoolittle@gmail.com>
2020-04-16 17:23:03 -0400

    replaces input text containing 'the' with 'BE'

--- src/ReplaceCommonWord.java
+++ src/ReplaceCommonWord.java
@@ -4,14 +4,44 @@ import Arrays
 declare class
 declare method
 declare vars for the, be, BE, input, splitstring
-begin do while loop
 prompt user to enter text
 initialize input to string
 split string
+while loop
 begin for loop
 if string[i] is the, replace it with BE
 print new string
-    exit when q
+recursive call on main method
+    exit when quit
  */
+
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class ReplaceCommonWord {
+    public static void main(String[] args) {
+        String the, be, quit, inputText, outputText;
+        String[] splitInputText;
+        Scanner input = new Scanner(System.in);
+        quit = "quit";
+        the = "the";
+        be = "BE";
+        outputText = "";
+
+        System.out.println("Enter some text or enter 'quit' to quit: ");
+        inputText = input.nextLine();
+        splitInputText = inputText.split(" ");
+
+        while (!inputText.equals(quit)) {
+            for (int i = 0; i < splitInputText.length; i ++) {
+                if (splitInputText[i].equals(the)) {
+                    splitInputText[i] = be;
+                }
+                outputText += splitInputText[i] + " ";
+            }
+            System.out.println(outputText);
+            main(new String[] {});
+            return;
+        }
+    }
 }
