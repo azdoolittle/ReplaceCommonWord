ffd582d5676791d4ac90deb51c01aa267846bc83
azdoolittle <drewdoolittle@gmail.com>
2020-04-16 16:16:48 -0400

    pseudocode only

---
+++ src/ReplaceCommonWord.java
@@ -0,0 +1,17 @@
+/*
+import scanner
+import Arrays
+declare class
+declare method
+declare vars for the, be, BE, input, splitstring
+begin do while loop
+prompt user to enter text
+initialize input to string
+split string
+begin for loop
+if string[i] is the, replace it with BE
+print new string
+    exit when q
+ */
+public class ReplaceCommonWord {
+}
